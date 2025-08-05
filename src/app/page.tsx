3c18b2615b6bc7d6ccc83e1a4295c607025f1915
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-08-05 22:54:42 +0700

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen min-w-screen overflow-x-hidden flex items-center justify-center">
+      <h1 className="text-9xl text-center w-full">HALLOO SELAMAT BEKERJA ADIK ADIK !</h1>
+    </div>
+  );
+}
