32b869ca1b1be51fe53086dd871d10476b6d8544
felix <ffelixrichardo@gmail.com>
2025-08-13 23:42:25 +0700

    mobile view ok, and font family ok

--- src/app/submission/page.tsx
+++ src/app/submission/page.tsx
@@ -67,20 +67,41 @@ const SubmissionPage = () => {
 
 
 
-          <div className='flex justify-center items-center min-h-[80vh] w-full'>
-            <div className='relative m-auto flex flex-col w-[50rem] h-[28rem] items-start px-28 pt-10'>
-                <Image
-                    src="/submission/background-card-submission.svg"
-                    alt="Submission Page"
-                    width={300}
-                    height={200}
-                    className="absolute -z-1 top-0 left-0 right-0 bottom-0 w-full h-auto"
-                    draggable={false}
-                />
+            <div className='flex justify-center items-center min-h-[80vh] w-full'>
+            <div className='relative m-auto flex flex-col w-[22rem] sm:w-[60vw] md:w-[50rem] h-[28rem] items-start px-15 md:px-28 pt-15'>
+              {/* <Image
+                src="/submission/background-card-submission.svg"
+                alt="Submission Page"
+                width={300}
+                height={200}
+                className="absolute -z-1 top-0 left-0 right-0 bottom-0 w-full h-auto"
+                draggable={false}
+                sizes="(max-width: 639px) 100vw, 50rem"
+              /> */}
 
-                <h1 className=' text-[#FCF551] font-rubik-glitch text-[4rem] text-shadow-[-4px_0px_0px_#D379F1] m-0'>Submission</h1>
-                <p className=' text-white'><b>Opened: </b> Sunday, 28 September 2025</p>
-                <p className=' text-white'><b>Due: </b> Monday, 29 September 2025</p>
+              {/* Desktop/Laptop Image */}
+              <Image
+                src="/submission/background-card-submission.svg"
+                alt="Submission Page"
+                width={300}
+                height={200}
+                className="absolute -z-1 top-0 left-0 right-0 bottom-0 w-full h-auto hidden md:block"
+                draggable={false}
+              />
+
+              {/* Mobile Image */}
+              <Image
+                src="/submission/background-card-mobile.svg"
+                alt="Submission Page Mobile"
+                width={300}
+                height={200}
+                className="absolute -z-1 top-0 left-0 right-0 bottom-0 w-full h-auto block md:hidden"
+                draggable={false}
+              />
+
+                <h1 className=' text-[#FCF551] font-rubik-glitch text-[2em] text-shadow-[-4px_0px_0px_#D379F1] m-0'>Submission</h1>
+                <p className='font-space-mono text-white'><b>Opened: </b> Sunday, 28 September 2025</p>
+                <p className='font-space-mono text-white'><b>Due: </b> Monday, 29 September 2025</p>
                 <Image
                   className='aspect-[1/1] mt-4 ml-0'
                   src="/submission/code-logo.svg"
@@ -88,7 +109,7 @@ const SubmissionPage = () => {
                   width={120}
                   height={200}
                 />
-                <div className='flex w-full justify-end items-center '>
+                <div className='flex w-full justify-end items-center gap-1 md:gap-3'>
                   <Button type={1} width='170px'  isDisabled={true}>Remove</Button>
                   <Button type={2} width='170px'  isDisabled={false}>Submit</Button>
                 </div>
